09c71bfa6ffe87d62cbafdd3bdb2461aba2e89d0
VolkodavAD <42648504+VolkodavAD@users.noreply.github.com>
2022-10-16 20:23:35 +0600

    Dev_Geodata
    Work this GeoDataSystem

--- Source/speedup/Public/GeoData/Speedup_GeoDataSystem.h
+++ Source/speedup/Public/GeoData/Speedup_GeoDataSystem.h
@@ -14,8 +14,8 @@
 //#include "AzureSpatialAnchorsTypes.h"
 #include "Speedup_GeoDataSystem.generated.h"
 
-class ULocationServicesImpl; 
-class ULocationServices;
+//class ULocationServicesImpl; 
+//class ULocationServices;
 
 UCLASS(Blueprintable, ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class SPEEDUP_API USpeedup_GeoDataSystem : public UActorComponent
@@ -26,20 +26,32 @@ public:
 	// Sets default values for this component's properties
 	USpeedup_GeoDataSystem();
 
+
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
+
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FMyBindableEvent);
+	
+	UPROPERTY(BlueprintAssignable)
+	FMyBindableEvent OnAwesomeness;
+	// Create Event
+	UFUNCTION(BlueprintImplementableEvent, Category = "BaseCharacter")
+	void OnDeath();
+	void OnDeath_Implementation();
 
-	void RepeatingFunction();
-
+	UFUNCTION(BlueprintImplementableEvent, Category = "GeoData")
+	void UpdateLocation();
+	void UpdateLocation_Implementation();
 	UFUNCTION(BlueprintCallable)
-	bool InitServis();
+	bool InitService();
 	UFUNCTION(BlueprintCallable)
-	bool StartServis();
+	bool StartService();
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "GeoData")
 	FGeoPointInfo GetLastLocation();
 
@@ -48,23 +60,29 @@ public:
 	UFUNCTION(BlueprintCallable)
 	void StopPath(FTimerHandle CurrentTimerH);
 
+	UFUNCTION(BlueprintCallable)
+	void StartPathInSneckers();
+	UFUNCTION(BlueprintCallable)
+	void StopPathInSneckers();
+
 public:
 	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "GeoData")
-	ULocationServices* SpeedupULocationServices;
+	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "GeoData")
+	//class ULocationServices *SpeedupULocationServices;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "GeoData")
-	ULocationServicesImpl* SpeedupULocationServicesImpl;
+	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "GeoData")
+	//ULocationServicesImpl* SpeedupULocationServicesImpl;
 
 	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "GeoData")
 	//class ULocationServicesImpl* SpeedUp_ULocationServices;
 
+
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "BaseData")
-	bool ServisEnable;
+		bool ServiceEnable;
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "BaseData")
-	bool ServisInit;
+		bool ServiceInit;
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "BaseData")
-	bool ServisStart;
+		bool ServiceStart;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "BaseData")
 	FTimerHandle Sneakers_TimerHandle;
@@ -79,4 +97,13 @@ public:
 	UGeoPath* ActivCarPath;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BaseData")
 	UGeoPath* ActivPlanePath;
+
+
+private:
+	UFUNCTION(BlueprintCallable)
+		void SetServiceEnable(bool value);
+	UFUNCTION(BlueprintCallable)
+		void SetServiceStart(bool value);
+	UFUNCTION(BlueprintCallable)
+		void SetServiceInit(bool value);
 };
